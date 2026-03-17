c476f1954d7815a73695ddc6df8189319d18ce51
Lee <lee@ip-192-168-193-193.us-west-2.compute.internal>
2026-03-16 19:26:39 -0700

    Fix decimal formatting issues and test. The 'frac' component won't always be zero due to floating point but strconv.FormatFloat will round to 3 places and can output a whole number.  Checking for a decimal at the end is more reliable to detect the rounding

--- decimal.go
+++ decimal.go
@@ -22,23 +22,19 @@ func marshalDecimal(b io.StringWriter, d float64) error {
 	const TH = 0.001
 
 	rounded := math.RoundToEven(d/TH) * TH
-	i, frac := math.Modf(rounded)
+	i, _ := math.Modf(rounded)
 
 	if i < -999999999999 || i > 999999999999 {
 		return ErrInvalidDecimal
 	}
 
-	if _, err := b.WriteString(strings.TrimRight(strconv.FormatFloat(rounded, 'f', 3, 64), "0")); err != nil {
-		return err
+	s := strings.TrimRight(strconv.FormatFloat(rounded, 'f', 3, 64), "0")
+	// Ensure at least one digit after the decimal point.
+	if s[len(s)-1] == '.' {
+		s += "0"
 	}
-
-	if frac == 0 {
-		_, err := b.WriteString("0")
-
-		return err
-	}
-
-	return nil
+	_, err := b.WriteString(s)
+	return err
 }
 
 func parseDecimal(s *scanner, decSepOff int, str string, neg bool) (float64, error) {
